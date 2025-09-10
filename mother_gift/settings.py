94958a1fa97e0b2cf93565f48e00adf455462202
MacBookPro16 <rrirrirri08@gmail.com>
2025-09-11 00:38:09 +0300

    feat: added new db configuration (will be hidden soon)

--- mother_gift/settings.py
+++ mother_gift/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'mother_gift.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mother_gift",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
