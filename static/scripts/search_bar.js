d8afcbb005acd974f2b54299272e679ffa86b831
MacBookPro16 <rrirrirri08@gmail.com>
2025-09-15 20:36:54 +0300

    feat: did the search bar

--- static/scripts/search_bar.js
+++ static/scripts/search_bar.js
@@ -1,8 +1,11 @@
-document.getElementById('search-icon').addEventListener('click', function () {
-    let searchBar = document.getElementById('search-bar');
-    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
-        searchBar.style.display = 'block';
-    } else {
-        searchBar.style.display = 'none';
-    }
-});
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('search-bar').style.display = 'none'
+    document.getElementById('search-icon').addEventListener('click', function () {
+        let searchBar = document.getElementById('search-bar');
+        if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+            searchBar.style.display = 'block';
+        } else {
+            searchBar.style.display = 'none';
+        }
+    });
+})
