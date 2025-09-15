542f2f58b89d3fe84993c2f0a109a5b86bbf9152
MacBookPro16 <rrirrirri08@gmail.com>
2025-09-15 22:16:34 +0300

    feat: finnaly extended it

--- mother_gift/settings.py
+++ mother_gift/settings.py
@@ -127,13 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+AUTH_USER_MODEL = "accounts.User"
+
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-AUTH_USER_MODEL = 'accounts.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
