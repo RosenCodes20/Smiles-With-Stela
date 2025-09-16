28b0e3f1f6e4a223614a39ed505c3261221843e8
MacBookPro16 <rrirrirri08@gmail.com>
2025-09-16 20:58:44 +0300

    feat: added the register view.

--- mother_gift/accounts/urls.py
+++ mother_gift/accounts/urls.py
@@ -4,5 +4,5 @@ from mother_gift.accounts import views
 
 urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
-    path('register/', views.register, name='register'),
+    path('register/', views.Register.as_view(), name='register'),
 ]
--- mother_gift/accounts/views.py
+++ mother_gift/accounts/views.py
@@ -2,14 +2,35 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from mother_gift.accounts.forms import UserRegisterForm
 
 # Create your views here.
 
 UserModel = get_user_model()
 
-class Login(LoginView):
+class Login(UserPassesTestMixin, LoginView):
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return False
+
+        elif not self.request.user.is_authenticated:
+            return True
 
     template_name = "login.html"
 
-def register(request):
-    return render(request, 'registration.html')
+class Register(UserPassesTestMixin, CreateView):
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return False
+
+        elif not self.request.user.is_authenticated:
+            return True
+
+    template_name = "registration.html"
+    model = UserModel
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login')
