9f56e8a973af4c0d6af96382eed2747273bd8b74
MacBookPro16 <rrirrirri08@gmail.com>
2025-09-16 23:16:17 +0300

    feat: managed to do the nav when a guest is logged in!

--- mother_gift/settings.py
+++ mother_gift/settings.py
@@ -138,6 +138,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+LOGIN_REDIRECT_URL = 'index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
