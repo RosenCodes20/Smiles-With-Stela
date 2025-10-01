bda168402c9179f3b49baeb2a3fd5e04db99fdb3
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-01 21:21:14 +0300

    feat: did the logic for gift_sets view

--- mother_gift/gift_sets/views.py
+++ mother_gift/gift_sets/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from mother_gift.all_products.models import AllProducts
+
+
 # Create your views here.
 
 def gift_sets(request):
 
-    return render(request, 'gift_sets.html')
+    gift_sets_queryset = AllProducts.objects.filter(product_type='Подаръчни комплекти')
+
+    context = {
+        'gift_sets_queryset': gift_sets_queryset
+    }
+
+    return render(request, 'gift_sets.html', gift_sets_queryset)
