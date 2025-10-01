3cd9d88db86f59bb29ce00fcbc4830fe041caf35
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-01 21:17:32 +0300

    feat: did the soaps view logic.

--- mother_gift/soaps/views.py
+++ mother_gift/soaps/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from mother_gift.all_products.models import AllProducts
+
+
 # Create your views here.
 
 def soaps(request):
 
-    return render(request, 'soaps.html')
+    soaps_queryset = AllProducts.objects.filter(product_type='Сапуни')
+
+    context = {
+        'soaps_queryset': soaps_queryset
+    }
+
+    return render(request, 'soaps.html', context)
