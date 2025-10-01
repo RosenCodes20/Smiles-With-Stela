c713b9bfed16d41f9818e2c7961a94073fda36b3
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-01 21:39:05 +0300

    feat: did the logic for view decorations

--- mother_gift/decorations/views.py
+++ mother_gift/decorations/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from mother_gift.all_products.models import AllProducts
+
+
 # Create your views here.
 
 def decorations(request):
 
-    return render(request, 'decorations.html')
+    decorations_queryset = AllProducts.objects.filter(product_type='Украси')
+
+    context = {
+        'decorations_queryset': decorations_queryset
+    }
+
+    return render(request, 'decorations.html', context)
