7196eaa0995f0c4468b5ad497be36e9ea85f88ae
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-02 18:44:44 +0300

    chore: changed all the fields in car model

--- mother_gift/cart/models.py
+++ mother_gift/cart/models.py
@@ -1,22 +1,26 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from decimal import Decimal
 
-class Product(models.Model):
-    name = models.CharField(max_length=500)
-    price_in_lv = models.DecimalField(max_digits=10, decimal_places=2)
-    price_in_eu = models.DecimalField(max_digits=10, decimal_places=2)
+from mother_gift.all_products.models import AllProducts
 
-    def __str__(self):
-        return self.name
+UserModel = get_user_model()
 
-class Cart(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+class Cart(AllProducts):
+    product_image_cat = models.ImageField(
+        upload_to=""
+    )
 
-    @property
-    def sum_in_lv(self):
-        return self.product.price_in_lv * Decimal(self.quantity)
+    product_description_cart = models.CharField(
+        max_length=200,
+    )
 
-    @property
-    def sum_in_eu(self):
-        return self.product.price_in_eu * Decimal(self.quantity)
+    product_price_cart = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    user_cart = models.ForeignKey(
+        to=UserModel,
+        on_delete=models.CASCADE
+    )
