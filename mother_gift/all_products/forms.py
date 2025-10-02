d889354c0542eb3c34fa80ae7fb4b1dbe568d983
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-02 17:38:37 +0300

    feat: added three more fields to the form

--- mother_gift/all_products/forms.py
+++ mother_gift/all_products/forms.py
@@ -6,4 +6,4 @@ from mother_gift.all_products.models import AllProducts
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = AllProducts
-        fields = ('product_image', 'product_description', 'product_type', 'product_price', 'is_available')
+        fields = ('product_image', 'second_product_image', 'first_long_description', 'second_long_description', 'applicable_for', 'product_description', 'product_type', 'product_price', 'is_available')
