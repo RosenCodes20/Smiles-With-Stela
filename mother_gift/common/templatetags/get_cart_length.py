4e8147f279959136e83e31f5adba0be6f4714894
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-06 21:34:35 +0300

    fix: fixed the template tag to be fine for every user

--- mother_gift/common/templatetags/get_cart_length.py
+++ mother_gift/common/templatetags/get_cart_length.py
@@ -4,6 +4,12 @@ from mother_gift.cart.models import Cart
 
 register = template.Library()
 
-@register.simple_tag()
-def get_cart_length():
-    return Cart.objects.all().count()
+@register.simple_tag(takes_context=True)
+def get_cart_length(context):
+    request = context['request']
+
+    if request.user.is_authenticated:
+        return Cart.objects.filter(user_cart=request.user).count()
+
+    else:
+        return 0
