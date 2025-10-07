542b423cd9b93aca528f98f98842ddea0a0c6eb8
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-07 21:16:19 +0300

    feat: removed the labels from every field in the form

--- mother_gift/cart/forms.py
+++ mother_gift/cart/forms.py
@@ -6,11 +6,16 @@ from mother_gift.cart.models import Addresses
 class BaseCartForm(forms.ModelForm):
     class Meta:
         model = Addresses
-        fields = "__all__"
+        fields = ('town_name', 'speedy_address')
 
         widgets = {
             "town_name": forms.TextInput(attrs={'placeholder': 'Моля въведете името на вашия град'}),
             'speedy_address': forms.TextInput(attrs={'placeholder': 'Моля въведете адресът на speedy във вашия град'})
+        }
+
+        labels = {
+            'town_name': '',
+            'speedy_address': ''
         }
 
 class CreateCartForm(BaseCartForm):
