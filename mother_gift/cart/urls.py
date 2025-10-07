d92cb79290e697855950c1a389103c0cf5d1f4b7
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-07 21:28:27 +0300

    feat: added new url

--- mother_gift/cart/urls.py
+++ mother_gift/cart/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add-to-cart/<int:pk>/', views.add_to_cart, name='add-to-cart'),
     path('remove-product/<int:pk>/', views.remove_product_from_cart, name='remove-cart-product'),
     path('finish_cart/', views.create_deliver_cart, name='finish-cart'),
+    path('thanks-for-choosing-this-page/', views.thanks_for_choosing, name='thanks-for-choosing')
 ]
