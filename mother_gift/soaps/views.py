1b8e4e1f8a318cea4c946238e230ac40a2f6fec5
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-12 20:30:37 +0300

    fix: fixed the problems with soaps view too

--- mother_gift/soaps/views.py
+++ mother_gift/soaps/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
+from mother_gift.all_products.function_helper import paginator_function_helper
 from mother_gift.all_products.models import AllProducts
 
 
@@ -10,20 +11,7 @@ def soaps(request):
 
     soaps_queryset = AllProducts.objects.filter(product_type='Сапуни')
 
-    products_per_page = 8
-
-    paginator = Paginator(soaps_queryset, products_per_page)
-
-    page = request.GET.get('page')
-
-    try:
-        soaps_queryset = paginator.page(page)
-
-    except PageNotAnInteger:
-        soaps_queryset = paginator.page(1)
-
-    except EmptyPage:
-        soaps_queryset = paginator.page(paginator.num_pages)
+    soaps_queryset = paginator_function_helper(request, soaps_queryset)
 
     context = {
         'soaps_queryset': soaps_queryset
