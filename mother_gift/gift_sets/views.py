9f6fd1fc56fecc2deb8629ab18206237935d00c5
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-12 20:35:17 +0300

    feat: implemented the logic in gift sets view

--- mother_gift/gift_sets/views.py
+++ mother_gift/gift_sets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from mother_gift.all_products.function_helper import paginator_function_helper
 from mother_gift.all_products.models import AllProducts
 
 
@@ -8,6 +9,8 @@ from mother_gift.all_products.models import AllProducts
 def gift_sets(request):
 
     gift_sets_queryset = AllProducts.objects.filter(product_type='Подаръчни комплекти')
+
+    gift_sets_queryset = paginator_function_helper(request, gift_sets_queryset)
 
     context = {
         'gift_sets_queryset': gift_sets_queryset
