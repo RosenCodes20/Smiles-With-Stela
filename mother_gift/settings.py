d197ab284ff7b77f742f16e07b90fd4aa6eb0a0c
MacBookPro16 <rrirrirri08@gmail.com>
2025-10-19 16:49:20 +0300

    feat: added facebook providers to allauth

--- mother_gift/settings.py
+++ mother_gift/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
     'django.contrib.sites',
 ]
 
