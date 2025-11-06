78aff8f190819f6203c9e26a847efaf6afed9fc5
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-06 09:27:39 +0200

    feat: added the implementation of the star review form

--- mother_gift/all_products/forms.py
+++ mother_gift/all_products/forms.py
@@ -7,3 +7,6 @@ class AddProductForm(forms.ModelForm):
     class Meta:
         model = AllProducts
         fields = ('product_image', 'second_product_image', 'first_long_description', 'second_long_description', 'applicable_for', 'product_description', 'product_type', 'product_price', 'is_available')
+
+class StarReviewForm(forms.Form):
+    pass
