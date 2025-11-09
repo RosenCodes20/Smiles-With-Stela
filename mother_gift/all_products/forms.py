0b20a347750fdd945fdb6d9c1a818f202ac8dee0
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-09 17:35:45 +0200

    feat: changed forms.Form to modelForm

--- mother_gift/all_products/forms.py
+++ mother_gift/all_products/forms.py
@@ -8,15 +8,5 @@ class AddProductForm(forms.ModelForm):
         model = AllProducts
         fields = ('product_image', 'second_product_image', 'first_long_description', 'second_long_description', 'applicable_for', 'product_description', 'product_type', 'product_price', 'is_available')
 
-class StarReviewForm(forms.Form):
-    opinion_for_product = forms.CharField(
-        max_length=150,
-        widget=forms.TextInput(attrs={
-            "placeholder": "Въведи мнение за продука......."
-        })
-    )
-
-    rating = forms.ChoiceField(
-        choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')],
-        widget=forms.RadioSelect(attrs={"class": "star-rating"})
-    )
+class StarReviewForm(forms.ModelForm):
+    pass
