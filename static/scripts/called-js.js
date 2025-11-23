877ab4c23add4db9b74f8ea2174d6f38c718e15e
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-23 15:22:36 +0200

    feat: implemented the logic (i am the best : )

--- static/scripts/called-js.js
+++ static/scripts/called-js.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let called = document.getElementById("called");
+    let called = document.querySelectorAll("#called");
 
-    console.log(called);
-}
+    for (span of called) {
+        if (screen.width < 890) {
+            span.id = "none";
+        }
+
+        console.log(span);
+    }
+})
