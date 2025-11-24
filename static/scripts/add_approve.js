55f173a65bbdaae096b547dd43805e830330c45d
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-24 08:36:00 +0200

    feat: implemented the logic

--- static/scripts/add_approve.js
+++ static/scripts/add_approve.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let texts = document.querySelectorAll(".about-us-sect > p");
 
     for (let text of texts) {
-
+        if (screen.width < 900) {
+            text.textContent += " ✅";
+            console.log(text);
+        }
     }
 })
