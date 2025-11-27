0bb234e24ccb55ba92e06b3e2c58ad8e67124b2d
Rosen Ivanov <rrirrirri08@gmail.com>
2025-11-27 20:38:08 +0200

    feat: fixed problems with form

--- mother_gift/cart/forms.py
+++ mother_gift/cart/forms.py
@@ -9,11 +9,17 @@ class BaseCartForm(forms.ModelForm):
         fields = ('first_name', 'last_name', 'address_email', 'town_name', 'speedy_address')
 
         widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "Моля въведе вашето име", 'id': 'username'}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Моля въведе вашата фамилия", 'id': 'username'}),
+            "address_email": forms.EmailInput(attrs={"placeholder": "Моля въведете вашия имейл", "id": "username"}),
             "town_name": forms.TextInput(attrs={'placeholder': 'Моля въведете вашия град', 'id': 'username'}),
             'speedy_address': forms.TextInput(attrs={'placeholder': 'Моля въведете адресът на speedy във вашия град', 'id': 'username'})
         }
 
         labels = {
+            "first_name": '',
+            "last_name": '',
+            "address_email": '',
             'town_name': '',
             'speedy_address': ''
         }
