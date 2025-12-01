603e949b5df7f3773fcc23399b0f7ed3e4f52b4e
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-01 08:58:40 +0200

    feat: added search form in the burger nav

--- static/scripts/search_bar.js
+++ static/scripts/search_bar.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('search-bar').style.display = 'none'
+    document.getElementById('search-bar').style.display = 'none';
+
     document.getElementById('search-icon').addEventListener('click', function () {
         let searchBar = document.getElementById('search-bar');
+
         if (searchBar.style.display === 'none' || searchBar.style.display === '') {
             searchBar.style.display = 'block';
         } else {
             searchBar.style.display = 'none';
         }
     });
+
+    document.querySelector(".item-items > .registration > ul > #search-icon").addEventListener("click", function () {
+        let searchBar = document.querySelector('.item-items > .registration > ul > section');
+
+        if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+            searchBar.style.display = 'block';
+        } else {
+            searchBar.style.display = 'none';
+        }
+    })
 })
