83acf7578d26b90e7ebc64e3c9297fcab34e24ca
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-01 11:23:00 +0200

    feat: fixed the problems with url matching

--- mother_gift/accounts/urls.py
+++ mother_gift/accounts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from mother_gift.accounts import views
 
 urlpatterns = [
-    path('login/', views.Login.as_view(), name='login'),
-    path('register/', views.Register.as_view(), name='register'),
-    path('logout/', views.Logout.as_view(), name='logout'),
+    path('login/', views.Login.as_view(), name='login-by-me'),
+    path('register/', views.Register.as_view(), name='register-by-me'),
+    path('logout/', views.Logout.as_view(), name='logout-by-me'),
 
     path('profile/<int:pk>/', views.ProfileDetails.as_view(), name='profile-details'),
 
