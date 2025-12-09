74074851ceeba7332159f8caee3c0f40db64701c
Rosen Ivanov <rrirrirri08@gmail.com>
2025-12-09 22:40:19 +0200

    feat: added more

--- mother_gift/settings.py
+++ mother_gift/settings.py
@@ -154,7 +154,7 @@ LOGOUT_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
